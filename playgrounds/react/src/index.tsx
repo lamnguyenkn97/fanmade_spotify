e249c587e355ac95e45ca88ac19699a848c22cd2
Lam Nguyen <languyen@axon.com>
2023-10-26 18:45:52 +0700

    no message

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Button} from '@dse.spotify/react';
+import '@dse.spotify/scss/src/lib/button.css';
+
+
+ReactDOM.render(
+    <Button label="Click me!!!"/>,
+    document.getElementById('root'))
