417943b713d341bb7b9adc65ab087a7032d3b2a5
Lam Nguyen <languyen@axon.com>
2023-11-18 21:11:01 +0700

    no message

---
+++ packages/foundation/lib/spacing.js
@@ -0,0 +1,11 @@
+export const Spacing = {
+    xxxs: 'xxxs',
+    xxs: 'xxs',
+    xs: 'xs',
+    md: 'md',
+    lg: 'lg',
+    xl: 'xl',
+    xxl: 'xxl',
+    xxxl: 'xxxl',
+    sm: 'sm',
+};
---
+++ packages/foundation/src/spacing.ts
@@ -0,0 +1,12 @@
+export const Spacing = {
+    xxxs: 'xxxs',
+    xxs: 'xxs',
+    xs: 'xs',
+    md: 'md',
+    lg: 'lg',
+    xl: 'xl',
+    xxl: 'xxl',
+    xxxl: 'xxxl',
+    sm: 'sm',
+
+}
