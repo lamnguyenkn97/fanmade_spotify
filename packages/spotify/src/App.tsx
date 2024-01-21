4928dd9302e77ea47fce8196a9dd9d8806bb1efd
Lam Nguyen <languyen@axon.com>
2024-01-21 10:37:26 +0700

    implement signup

--- packages/spotify/src/App.tsx
+++ packages/spotify/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HomePage, LoginPage, SongListPage} from "@dse.spotify/spotify-components";
+import {HomePage, LoginPage, SongListPage, SignupPage} from "@dse.spotify/spotify-components";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import '@dse.spotify/scss/src/lib/button.css';
 import '@dse.spotify/scss/src/lib/textButton.css';
@@ -22,6 +22,8 @@ import '@dse.spotify/scss/src/lib/playlist.css';
 import '@dse.spotify/scss/src/lib/panel.css';
 import '@dse.spotify/scss/src/lib/playlistCover.css';
 import '@dse.spotify/scss/src/lib/song.css';
+import '@dse.spotify/scss/src/lib/signupPage.css';
+
 
 function App() {
     return (
@@ -30,6 +32,7 @@ function App() {
                 <Route path="">
                     <Route path='/' index element={<HomePage/>}/>
                     <Route path='/login' element={<LoginPage/>}/>
+                    <Route path='/signup' element={<SignupPage/>}/>
                     <Route path='/playlist' element={<SongListPage/>}/>
                 </Route>
             </Routes>
